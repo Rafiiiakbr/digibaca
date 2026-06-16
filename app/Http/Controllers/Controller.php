e04dd3d00143fc4011fa3053cd957f605f6e8cc6
MuhammadRafiAkbr <mhmmdrfi701@gmail.com>
2026-06-17 01:31:48 +0700

    FULLSTTACK

---
+++ app/Http/Controllers/Controller.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Http\Controllers;
+
+abstract class Controller
+{
+    // Ini adalah class induk kosong tempat Laravel mengatur sistem controller dasar.
+    // Semua controller lain (seperti AuthController, AdminController) akan mewarisi class ini.
+}
